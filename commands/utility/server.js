6c035a4ea45ac65b0c47a3fe65c6c6648c7d0d25
jirachixu <xu1838@purdue.edu>
2025-09-24 17:56:07 -0400

    fixed error when using certain commands in DM

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,11 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		try {
+			// interaction.guild is the object representing the Guild in which the command was run
+			await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		} catch (error) {
+			await interaction.reply({ content: 'An error occurred! Make sure you\'re not running this command in a DM!', flags: MessageFlags.Ephemeral });
+		}
 	},
 };
