62a440c89207744810860c2eb4f8a35922394d95
jirachixu <xu1838@purdue.edu>
2025-09-25 17:46:38 -0400

    better login detection

--- commands/users/mylist.js
+++ commands/users/mylist.js
@@ -33,124 +33,108 @@ module.exports = {
             const status = interaction.options.getString('status');
             const sort = interaction.options.getString('sort') || 'list_updated_at';
             const uid = String(interaction.user.id);
-            let nonExistent = false;
-            let isError = false;
             
             fs.readFile('./access_tokens.json', 'utf8', async (err, data) => {
                 if (err) {
-                    isError = true;
+                    await interaction.reply({ content: 'An error occurred while accessing your tokens. Please try again later.', flags: MessageFlags.Ephemeral });
                     return;
                 }
-                try {
-                    const tokens = JSON.parse(data);
-                    if (!(uid in tokens)) {
-                        nonExistent = true;
-                        return;
-                    }
-
-                    const response = await fetch(`https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status&sort=${sort}${status ? `&status=${status}` : ''}&limit=20`, {
-                        headers: {
-                            'Authorization': `Bearer ${tokens[uid]}`,
-                        }
-                    });
-                    
-                    if (!response.ok) {
-                        console.log(response.status);
-                        await interaction.reply({ content: 'Please log into MyAnimeList with /login.', flags: MessageFlags.Ephemeral });
-                        return;
-                    }
-
-                    const dt = await response.json();
-                    if (dt.status === 403 || dt.status === 401) {
-                        await interaction.reply({ content: 'Please log into MyAnimeList with /login.', flags: MessageFlags.Ephemeral });
-                        return;
-                    }
-
-                    if (!dt.data) {
-                        await interaction.reply({ content: 'This user doesn\'t exist!', flags: MessageFlags.Ephemeral });
-                        return;
-                    }
-
-                    const results = dt.data;
-                    const embeds = [];
-
-                    if (results.length === 0) {
-                        await interaction.reply({ content: 'This user has no anime in their list!', flags: MessageFlags.Ephemeral });
-                        return;
-                    }
-
-                    const reply = await interaction.deferReply();
-
-                    let i = 1;
-                    for (const result of results) {
-                        const embed = new EmbedBuilder()
-                            .setColor(0xff99dd)
-                            .setTitle(`${result.node.title ?? 'No Title'}`)
-                            .setFields(
-                                { name: 'ID', value: `${result.node.id ?? 'No ID'}`, inline: true }, 
-                                { name: 'Status', value: `${result.list_status.status ?? 'No Status'}`, inline: true }, 
-                                { name: 'Episodes Watched', value: `${result.list_status.num_episodes_watched}`, inline: true }, 
-                                { name: 'Score', value: `${result.list_status.score ?? 'Not Scored'}`, inline: true },
-                                { name: 'Rewatching', value: `${result.list_status.is_rewatching}`, inline: true },
-                                { name: 'Update Date', value: `${result.list_status.updated_at.substring(0, result.list_status.updated_at.indexOf('T'))}`, inline: true },
-                            )
-                            .setImage(`${result.node.main_picture.medium ?? ''}`)
-                            .setTimestamp()
-                            .setFooter({ text: `My Anime List  •  Page ${i} of ${results.length}` });
-                        embeds.push(embed);
-                        i++;
-                    }
-
-                    const prevButton = new ButtonBuilder()
-                        .setCustomId(`prev-${interaction.user.id}`)
-                        .setLabel('←')
-                        .setStyle(ButtonStyle.Secondary)
-                        .setDisabled(true);
-
-                    const nextButton = new ButtonBuilder()
-                        .setCustomId(`next-${interaction.user.id}`)
-                        .setLabel('→')
-                        .setStyle(ButtonStyle.Secondary)
-                        .setDisabled(embeds.length === 1);
-
-                    const row = new ActionRowBuilder().addComponents(prevButton, nextButton);
-
-                    let currentPage = 0;
-
-                    await interaction.editReply({ embeds: [embeds[currentPage]], components: [row] });
-
-                    const collector = reply.createMessageComponentCollector({
-                        filter: i => i.customId === `prev-${interaction.user.id}` || i.customId === `next-${interaction.user.id}`,
-                        time: 300000,
-                    });
-
-                    collector.on('collect', async i => {
-                        if (i.customId === `prev-${i.user.id}`) {
-                            currentPage--;
-                        } else if (i.customId === `next-${i.user.id}`) {
-                            currentPage++;
-                        }
-
-                        prevButton.setDisabled(currentPage === 0);
-                        nextButton.setDisabled(currentPage === embeds.length - 1);
-
-                        await i.update({ embeds: [embeds[currentPage]], components: [row] });
-                    });
-                } catch (error) {
-                    isError = true;
+                
+                const tokens = JSON.parse(data);
+                if (!(uid in tokens)) {
+                    await interaction.reply({ content: 'You need to link your MyAnimeList account first using /login.', flags: MessageFlags.Ephemeral });
                     return;
                 }
+
+                const response = await fetch(`https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status&sort=${sort}${status ? `&status=${status}` : ''}&limit=20`, {
+                    headers: {
+                        'Authorization': `Bearer ${tokens[uid]}`,
+                    }
+                });
+                
+                if (!response.ok) {
+                    console.log(response.status);
+                    await interaction.reply({ content: 'Please log into MyAnimeList with /login.', flags: MessageFlags.Ephemeral });
+                    return;
+                }
+
+                const dt = await response.json();
+                if (dt.status === 403 || dt.status === 401) {
+                    await interaction.reply({ content: 'Please log into MyAnimeList with /login.', flags: MessageFlags.Ephemeral });
+                    return;
+                }
+
+                if (!dt.data) {
+                    await interaction.reply({ content: 'This user doesn\'t exist!', flags: MessageFlags.Ephemeral });
+                    return;
+                }
+
+                const results = dt.data;
+                const embeds = [];
+
+                if (results.length === 0) {
+                    await interaction.reply({ content: 'This user has no anime in their list!', flags: MessageFlags.Ephemeral });
+                    return;
+                }
+
+                const reply = await interaction.deferReply();
+
+                let i = 1;
+                for (const result of results) {
+                    const embed = new EmbedBuilder()
+                        .setColor(0xff99dd)
+                        .setTitle(`${result.node.title ?? 'No Title'}`)
+                        .setFields(
+                            { name: 'ID', value: `${result.node.id ?? 'No ID'}`, inline: true }, 
+                            { name: 'Status', value: `${result.list_status.status ?? 'No Status'}`, inline: true }, 
+                            { name: 'Episodes Watched', value: `${result.list_status.num_episodes_watched}`, inline: true }, 
+                            { name: 'Score', value: `${result.list_status.score ?? 'Not Scored'}`, inline: true },
+                            { name: 'Rewatching', value: `${result.list_status.is_rewatching}`, inline: true },
+                            { name: 'Update Date', value: `${result.list_status.updated_at.substring(0, result.list_status.updated_at.indexOf('T'))}`, inline: true },
+                        )
+                        .setImage(`${result.node.main_picture.medium ?? ''}`)
+                        .setTimestamp()
+                        .setFooter({ text: `My Anime List  •  Page ${i} of ${results.length}` });
+                    embeds.push(embed);
+                    i++;
+                }
+
+                const prevButton = new ButtonBuilder()
+                    .setCustomId(`prev-${interaction.user.id}`)
+                    .setLabel('←')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(true);
+
+                const nextButton = new ButtonBuilder()
+                    .setCustomId(`next-${interaction.user.id}`)
+                    .setLabel('→')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(embeds.length === 1);
+
+                const row = new ActionRowBuilder().addComponents(prevButton, nextButton);
+
+                let currentPage = 0;
+
+                await interaction.editReply({ embeds: [embeds[currentPage]], components: [row] });
+
+                const collector = reply.createMessageComponentCollector({
+                    filter: i => i.customId === `prev-${interaction.user.id}` || i.customId === `next-${interaction.user.id}`,
+                    time: 300000,
+                });
+
+                collector.on('collect', async i => {
+                    if (i.customId === `prev-${i.user.id}`) {
+                        currentPage--;
+                    } else if (i.customId === `next-${i.user.id}`) {
+                        currentPage++;
+                    }
+
+                    prevButton.setDisabled(currentPage === 0);
+                    nextButton.setDisabled(currentPage === embeds.length - 1);
+
+                    await i.update({ embeds: [embeds[currentPage]], components: [row] });
+                });
             });
-
-            if (isError) {
-                await interaction.reply({ content: 'An error occurred while accessing your tokens. Please try again later.', flags: MessageFlags.Ephemeral });
-                return;
-            }
-
-            if (nonExistent) {
-                await interaction.reply({ content: 'You need to link your MyAnimeList account first using /login.', flags: MessageFlags.Ephemeral });
-                return;
-            }            
         } catch (error) {
             await interaction.editReply({ content: 'An error occurred! (Most likely API rate limit)', flags: MessageFlags.Ephemeral });
             console.error(error);
